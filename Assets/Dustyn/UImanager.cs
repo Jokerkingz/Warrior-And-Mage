9a5b9ca375ee74564e7d1d0c9ae444358e84345b
DAWSONCOLLEGE\1637706 <dustyn.alberts@hotmail.com>
2017-09-05 21:59:56 -0400

    UI and Stats WIP
    Added UI and some stats (use number keys and up and down to change values)

---
+++ Assets/Dustyn/UImanager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour {
+
+	//UI
+	[Header("Warrior UI References")]
+	public Slider healthBarWarrior;
+	public Text healthTextWarrior;
+	public Slider staminaBar;
+	public Text staminaText;
+
+	public Slider warriorExpBar;
+	public Text warriorExpText;
+	public Text warriorLevel;
+
+	[Header("Mage UI References")]
+	public Slider healthBarMage;
+	public Text healthTextMage;
+	public Slider manaBar;
+	public Text manaText;
+
+	public Slider mageExpBar;
+	public Text mageExpText;
+	public Text mageLevel;
+
+	//Characters
+	[Header("Characters References")]
+	public statManager smWarrior;
+	public statManager smMage;
+
+	private static bool UIExist;
+
+	void Awake()
+	{
+		if (!UIExist) {
+			UIExist = true;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+
+	}
+
+	void Update () {
+		
+		//WARRIOR HEALTH
+		healthBarWarrior.maxValue = smWarrior.maxHealth;
+		healthBarWarrior.value = smWarrior.curHealth;
+		healthTextWarrior.text = "HEALTH: " + smWarrior.curHealth + "/" + smWarrior.maxHealth;
+	
+		//MAGE HEALTH
+		healthBarMage.maxValue = smMage.maxHealth;
+		healthBarMage.value = smMage.curHealth;
+		healthTextMage.text = "HEALTH: " + smMage.curHealth + "/" + smMage.maxHealth;
+
+		//STAMINA
+		staminaBar.maxValue = smWarrior.maxSpec;
+		staminaBar.value = smWarrior.curSpec;
+		staminaText.text = "STAMINA: " + smWarrior.curSpec + "/" + smWarrior.maxSpec;
+
+		//MANA
+		manaBar.maxValue = smMage.maxSpec;
+		manaBar.value = smMage.curSpec;
+		manaText.text = "MANA: " + smMage.curSpec + "/" + smMage.maxSpec;
+
+
+		//WARRIOR LEVEL UP
+		warriorExpBar.maxValue =smWarrior.maxExp;
+		warriorExpBar.value = smWarrior.curExp;
+		warriorExpText.text = "XP: " + smWarrior.curExp + "/" + smWarrior.maxExp;
+		warriorLevel.text = "WARRIOR (LEVEL " + smWarrior.curLvl + ")";
+
+		//MAGE LEVEL UP
+		mageExpBar.maxValue =smMage.maxExp;
+		mageExpBar.value = smMage.curExp;
+		mageExpText.text = "XP: " + smMage.curExp + "/" + smMage.maxExp;
+		mageLevel.text = "MAGE (LEVEL " + smMage.curLvl + ")";
+	}
+		
+}
---
+++ Assets/Dustyn/statManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class statManager : MonoBehaviour {
+
+	//public GameObject target;
+
+	public int maxHealth;
+	public int curHealth;
+
+	//NOTE: Spec refers to either Stamina or Mana
+	public int maxSpec;
+	public int curSpec;
+
+	public int maxExp;
+	public int curExp;
+	public int maxLvl;
+	public int curLvl;
+
+	public KeyCode DrainHealth;
+	public KeyCode ReplenishHealth;
+	public KeyCode DrainSpec;
+	public KeyCode ReplenishSpec;
+
+	public KeyCode AddXP;
+
+	void Start () {
+		
+		curHealth = maxHealth;
+		curSpec = maxSpec;
+		curExp = 0;
+	}
+	
+
+	void Update () {
+
+
+
+		// FOR TESTING PURPOSES
+
+		if (Input.GetKeyDown (DrainHealth)) {
+			curHealth -= 10;
+		}
+		if (Input.GetKeyDown (ReplenishHealth)) {
+			curHealth += 10;
+		}
+		if (Input.GetKeyDown (DrainSpec)) {
+			curSpec -= 10;
+		}
+		if (Input.GetKeyDown (ReplenishSpec)) {
+			curSpec += 10;
+		}
+
+		if (Input.GetKeyDown (AddXP)) {
+			curExp += 10;
+		}
+
+		Stats ();
+	}
+
+	void Stats()
+	{
+		if (curHealth <= 0) {
+			curHealth = 0;
+		}
+		if (curHealth > maxHealth) {
+			curHealth = maxHealth;
+
+		}if (curSpec <= 0) {
+			curSpec = 0;
+		}
+		if (curSpec > maxSpec) {
+			curSpec = maxSpec;
+		}
+			
+		if (curExp >= maxExp) {
+			curLvl += 1;
+			curExp = 0;
+			maxExp *= 2;
+			maxHealth += 50;
+			maxSpec +=50;
+			curHealth += 50;
+			curSpec += 50;
+		}
+
+		if (curLvl >= maxLvl) {
+			curLvl = maxLvl;
+		}
+}
+}
