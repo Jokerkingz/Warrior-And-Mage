404cd07cd67c0cbd4b7d7a80304e9ada75e8772d
DAWSONCOLLEGE\1637706 <dustyn.alberts@hotmail.com>
2017-09-11 20:48:55 -0400

    Auto stash before merge of "master" and "origin/master"
    UI controller fade in/fade out. Slide WIP.
    Warrior stat bars available (yet to be linked, still using test keys)
    
    error with the Global gameobject saying something about animation and the orc.

---
+++ Assets/Dustyn/controlDisplay.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlDisplay : MonoBehaviour {
+
+
+	public float alpha;
+	public float changeRate;
+	private CanvasGroup cg;
+
+	public bool active;
+	public bool canPress;
+
+	public KeyCode testButton;
+
+	public float startPos;
+	public float endPos;
+
+	//public string UIstatus;
+	//public float slideFrame;
+	//public float gotoX;
+
+	void Start () {
+		cg = this.gameObject.GetComponent<CanvasGroup> ();
+		active = true;
+		canPress = true;
+		transform.localPosition = new Vector3 (startPos, -11, 0);
+		//UIstatus = "controls";
+	
+	}
+
+	void Update()
+	{
+
+		/*switch (UIstatus) {
+		case "controls":
+			slideFrame += Time.deltaTime;
+			Mathf.Clamp (slideFrame, 0f, 1f);
+
+
+			break;
+		}
+		gotoX = Mathf.Lerp (0f, 100f, slideFrame);
+		gameObject.transform.localPosition = new Vector3 (gotoX, 0f, 0f);
+*/
+		if (Input.GetKeyDown (testButton) &&active==false &&canPress==true) {
+			active = true;
+			canPress = false;
+		}
+
+		if (Input.GetKeyDown (testButton) &&active==true &&canPress==true) {
+			active = false;
+			canPress = false;
+		}
+
+
+		if (active == true) {
+			cg.alpha = alpha += changeRate * Time.deltaTime;
+			//transform.localPosition = new Vector3.Lerp (startPos, -11f, 0f);
+		}
+		if (active == false) {
+			cg.alpha = alpha -= changeRate * Time.deltaTime;
+			//transform.localPosition = new Vector3.Lerp (endPos, -11f, 0f);
+		}
+
+
+		if (alpha >= 1f) 
+		{
+			canPress = true;
+			alpha = 1f;
+		}
+		if (alpha <=0f)
+		{
+			canPress = true;
+			alpha=0f;
+		}
+
+	}
+
+}
---
+++ Assets/Dustyn/statBarFade.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class statBarFade : MonoBehaviour {
+
+	private float alpha;
+	private CanvasGroup cg;
+
+	//YES, CHRIST, THESE ARE TIMERS, NOT A COROUTINE HAHAHAHA TRY TO STOP ME
+	private bool active;
+	private float atimer = 0f;
+	private float atime = 1f;
+
+	void Start () {
+		cg = this.gameObject.GetComponent<CanvasGroup> ();
+		active = false;
+	}
+
+	void Update () {
+
+		if (active == false) {
+			cg.alpha = alpha -= 0.5f * Time.deltaTime;
+		}
+
+		if (alpha <=0f)
+		{
+			active = false;
+			alpha=0f;
+		}
+
+		if (active == true) {
+			atimer += Time.deltaTime;
+		}
+		if (atimer >= atime) {
+			active = false;
+			atimer = 0f;
+		}
+
+	}
+
+	public void Appear()
+	{
+		cg.alpha = alpha = 1f;
+		active = true;
+
+	}
+}
