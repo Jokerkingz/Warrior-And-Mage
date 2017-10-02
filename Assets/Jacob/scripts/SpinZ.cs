4462f57909fb7d4ebcbfa06188bdea959c34af57
Jacoco <jacob.kelly343@gmail.com>
2017-10-02 17:24:56 -0400

    Pickup objects now rotate
    pffffff wtv

---
+++ Assets/Jacob/scripts/SpinZ.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinZ : MonoBehaviour
+{
+	
+
+
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (Time.deltaTime * 0,0, 2));
+	}
+}
