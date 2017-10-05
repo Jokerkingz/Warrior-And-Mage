50514161d36c1861265a8488b4a670206365ec86
Jacoco <jacob.kelly343@gmail.com>
2017-10-04 20:36:01 -0400

    Inventory WIP
    nothing is working my lil goo goo

---
+++ Assets/Jacob/scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	public List<GameObject> SpellInventory = new List <GameObject> ();
+
+	public void AddNewSpell(GameObject newSpell)
+	{
+		for(int i = 0; i <SpellInventory.Count; ++i)
+		{
+			if (SpellInventory [i].name == newSpell.name)
+				return;
+		}
+		SpellInventory.Add(newSpell);
+	}
+
+
+}
---
+++ Assets/Jacob/scripts/PowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+	public GameObject Spell;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Mage")
+			{
+				other.gameObject.GetComponent<Inventory>().AddNewSpell (Spell);
+			}
+	}
+}
