0562b7b796b60212e26fcb4cb63f6cfaa065f3a2
Unknown <ChristLaurenzs@gmail.com>
2017-10-12 20:33:50 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Scr_IceSpear.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_IceSpear : MonoBehaviour {
+	public Vector3 vStartPoint;
+	public Vector3 vTargetSpot;
+	public float tTemp;
+	// Use this for initialization
+	void Start () {
+		vStartPoint = transform.position;
+		Invoke("Die",10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt(vTargetSpot);
+		transform.Translate(Vector3.forward*4f*Time.deltaTime,Space.Self);
+		tTemp = Vector3.Distance(transform.position,vTargetSpot);
+		if (tTemp<.5f)
+			TurnOff();
+	}
+	void TurnOff(){
+		GetComponent<ParticleSystem>().Stop();
+		GetComponentInChildren<MeshRenderer>().enabled = false;
+		tag = "Untagged";
+	}
+	void Die(){
+		Destroy(this.gameObject);
+	}
+}
